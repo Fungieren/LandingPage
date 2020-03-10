afa0fa47c2e66c7e28109136e8806f305bbb1f8d
Fungieren <ye.fung.tchen@klqc.de>
2020-03-10 12:16:10 +0100

    Define global Variables

--- js/app.js
+++ js/app.js
@@ -11,48 +11,42 @@
  *
  * JS Standard: ESlint
  *
-*/
+ */
 
 /**
  * Define Global Variables
  *
-*/
+ */
 
-document.querySelectorAll('.landing__container');
-
+const unorderedList = document.querySelector("#navbar__list");
+const landingContainer = document.querySelectorAll(".landing__container");
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
-*/
-
-
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
  *
-*/
+ */
 
 // build the nav
 
-
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
  *
-*/
+ */
 
 // Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
